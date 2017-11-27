c0970c0995df23177ce8c3354007b904e36a83ee
root <bg.11@hotmail.com>
2017-11-27 10:48:44 +0000

    first

--- db.php
+++ db.php
@@ -6,7 +6,7 @@ if (!$link) {
 //echo 'Connected successfully';
 
 
-$webrtc = mysql_select_db("webrtc",$link) or die("Could not select webrtc");
+$webrtc = mysqli_select_db("webrtc",$link) or die("Could not select webrtc");
 
 
 
